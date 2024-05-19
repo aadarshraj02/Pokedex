a24503bbd74b1e5e44d89034916f881764acdf66
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-19 22:58:53 +0530

    added fetch and promises chanining

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const url = "https://pokeapi.co/api/v2/pokemon";
 
-const fetchPokemon = () => {};
+const fetchPokemon = () => {
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+};
+fetchPokemon();
