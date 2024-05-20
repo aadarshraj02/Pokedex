07be2033fb23faa62baeb2527f726d7b218ea15a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-20 10:04:23 +0530

    changing function to async and updated base url

--- script.js
+++ script.js
@@ -2,21 +2,8 @@
 
 const pokedex = document.getElementById("pokedex");
 
-const fetchPokemon = () => {
-  const promises = [];
-  for (let i = 1; i <= 150; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then((response) => response.json()));
-  }
-  Promise.all(promises).then((result) => {
-    const pokemon = result.map((data) => ({
-      name: data.name,
-      id: data.id,
-      image: data.sprites.front_default,
-      type: data.types.map((type) => type.type.name).toString(),
-    }));
-    displayPokemon(pokemon);
-  });
+const fetchPokemon = async () => {
+  const url = `https://pokeapi.co/api/v2/pokemon?limit =150`;
 };
 
 function displayPokemon(pokemon) {
