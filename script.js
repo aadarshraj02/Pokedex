9023afe0c7e2a7d862dd4244cb46da13e77ec047
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-20 10:06:57 +0530

    added await and stored response json in data variable

--- script.js
+++ script.js
@@ -3,7 +3,10 @@
 const pokedex = document.getElementById("pokedex");
 
 const fetchPokemon = async () => {
-  const url = `https://pokeapi.co/api/v2/pokemon?limit =150`;
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
 };
 
 function displayPokemon(pokemon) {
