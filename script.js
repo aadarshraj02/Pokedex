177cd2b75cd0012cc4d41c44e9ec4d7ad3a6b182
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-20 10:22:14 +0530

    added map property on result of data to get each pokemon data

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ const fetchPokemon = async () => {
   const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
+  const pokemon = data.results.map((result, index) => ({
+    name: data.name,
+    id: index + 1,
+    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+      index + 1
+    }.png`,
+  }));
+  displayPokemon(pokemon);
 };
 
 function displayPokemon(pokemon) {
